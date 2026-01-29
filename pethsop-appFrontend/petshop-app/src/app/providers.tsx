11f2de728f5d9e8a87b3ba2d3120032c12dea61e
Batuhan Çallıoğlu <batucalll3@gmail.com>
2026-01-29 16:21:19 +0300

    auth provider

--- pethsop-appFrontend/petshop-app/src/app/providers.tsx
+++ pethsop-appFrontend/petshop-app/src/app/providers.tsx
@@ -9,6 +9,7 @@ import Sidebar from "@/app/Sidebar/page";
 import { usePathname } from "next/navigation";
 import { SessionProvider } from "next-auth/react";
 import ScrollToTop from "./components/ScrollToTop";
+import "@/lib/axiosConfig";
 
 export default function Providers({
   children,
